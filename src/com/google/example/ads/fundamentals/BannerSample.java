3df7cfc0e067ec02481ffe82907cf9a3ed2ea9fb
Ahmed Castro <antares_43@hotmail.com>
2012-06-10 11:44:07 -0600

    First commit

---
+++ src/com/google/example/ads/fundamentals/BannerSample.java
@@ -0,0 +1,53 @@
+package com.google.example.ads.fundamentals;
+
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+/**
+ * A simple {@link Activity} that embeds an AdView.
+ */
+public class BannerSample extends Activity {
+  /** The view to show the ad. */
+  private AdView adView;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    // Create an ad.
+    adView = new AdView(this, AdSize.BANNER, "a14fd28a6ed31f2");
+
+    // Add the AdView to the view hierarchy. The view will have no size
+    // until the ad is loaded.
+    LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout);
+    layout.addView(adView);
+
+    // Create an ad request. Check logcat output for the hashed device ID to
+    // get test ads on a physical device.    
+    AdRequest adRequest = new AdRequest();
+    adRequest.addTestDevice(AdRequest.TEST_EMULATOR);
+    //TODO: change Test Device
+    adRequest.addTestDevice("DBBDC3E9E4BBAA1CC7CC67720C2AAF30");
+
+    // Start loading the ad in the background.
+    adView.loadAd(adRequest);
+  }
+
+  /** Called before the activity is destroyed. */
+  @Override
+  public void onDestroy() {
+    // Destroy the AdView.
+    if (adView != null) {
+      adView.destroy();
+    }
+
+    super.onDestroy();
+  }
+}
